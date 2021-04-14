7f3620e3287d66930e423a9d6c06ec7f3e2ca83d
davidzie <davidzi@post.bgu.ac.il>
2021-04-14 14:20:44 +0300

    Documentation and queues split. Communication Handler methods documentation. Split send and receive queues methods and add purge option.

--- src/main/java/Communication/CommunicationHandler.java
+++ src/main/java/Communication/CommunicationHandler.java
@@ -16,28 +16,93 @@ public class CommunicationHandler {
     private Connection connection;
     private DeliverCallback myCallback = this::onReceiveCallback;
 
-    public void openQueues() throws IOException, TimeoutException {
-        connection = factory.newConnection();
-
-        sendChannel = connection.createChannel();
+    /**
+     * Open Queue for sending messages
+     * @param purge existing messages in queue
+     * @throws IOException on connection error
+     * @throws TimeoutException on no response from RabbitMQ server
+     */
+    public void openSendQueue(boolean purge) throws IOException, TimeoutException {
+        setUpQueueOpening(sendChannel, sendQueueName, purge);
         sendChannel.queueDeclare(sendQueueName, false, false, false, null);
-
-        receiveChannel = connection.createChannel();
+    }
+    /**
+     * Open Queue for receiving messages
+     * @param purge existing messages in queue
+     * @throws IOException on connection error
+     * @throws TimeoutException on no response from RabbitMQ server
+     */
+    public void openReceiveQueue(boolean purge) throws IOException, TimeoutException {
+        setUpQueueOpening(receiveChannel, receiveQueueName, purge);
         receiveChannel.queueDeclare(receiveQueueName, false, false, false, null);
-
         receiveChannel.basicConsume(receiveQueueName, true, myCallback, consumerTag -> { });
     }
+    /**
+     * Initiate new connection if necessary.
+     * Close channel if it was already open, and create new one.
+     * Purge queue if requested
+     * @param purge existing messages on queue
+     * @throws IOException on connection error
+     * @throws TimeoutException on no response from RabbitMQ server
+     */
+    private void setUpQueueOpening(Channel channel, String queueName, boolean purge) throws IOException, TimeoutException {
+        if (connection == null){
+            connection = factory.newConnection();
+        }
 
+        if (channel != null){
+            channel.close();
+        }
+        channel = connection.createChannel();
+
+        if (purge){
+            channel.queuePurge(queueName);
+        }
+    }
+
+    /**
+     * Close Send queue
+     * @throws IOException on connection error
+     * @throws TimeoutException on no response from RabbitMQ server
+     */
+    public void closeSendQueue() throws IOException, TimeoutException {
+        sendChannel.close();
+    }
+
+    /**
+     * Close Receive queue
+     * @throws IOException on connection error
+     * @throws TimeoutException on no response from RabbitMQ server
+     */
+    public void closeReceiveQueue() throws IOException, TimeoutException {
+        receiveChannel.close();
+    }
+
+    /**
+     * Close Send and Receive connection
+     * @throws IOException on connection error
+     * @throws TimeoutException on no response from RabbitMQ server
+     */
     public void closeQueues() throws IOException, TimeoutException {
         sendChannel.close();
         receiveChannel.close();
         connection.close();
     }
 
+    /**
+     * Put message in Send queue
+     * @param message to send
+     * @throws IOException on connection error
+     */
     public void send(String message) throws IOException {
         sendChannel.basicPublish("", sendQueueName, null, message.getBytes());
     }
 
+    /**
+     * Default callback for receiving messages
+     * @param consumerTag Rabbimq consumer tag
+     * @param delivery object containing message and data
+     */
     private void onReceiveCallback(String consumerTag, Delivery delivery){
         String json = new String(delivery.getBody(), StandardCharsets.UTF_8);
         System.out.println(json);
