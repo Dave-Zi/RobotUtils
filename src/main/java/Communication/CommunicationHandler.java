0bf2d48cbb7b0f75bbf9d78e25fc80c27df7d351
davidzie <davidzi@post.bgu.ac.il>
2021-04-24 19:06:27 +0300

    Add Communication Interface Fix

--- src/main/java/Communication/CommunicationHandler.java
+++ src/main/java/Communication/CommunicationHandler.java
@@ -7,8 +7,10 @@ import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
-public class CommunicationHandler implements ICommunication{
+public class CommunicationHandler implements ICommunication {
 
+    private final String sosQueueName = "Sos";
+    private final int queueSize = 5;
     private Channel sendChannel;
     private Channel receiveChannel;
     private String sendQueueName = "Commands";
@@ -16,42 +18,17 @@ public class CommunicationHandler implements ICommunication{
     private ConnectionFactory factory = new ConnectionFactory();
     private Connection connection;
     private DeliverCallback myCallback = this::defaultCallback;
-
-    private final String sosQueueName = "Sos";
-    private final int queueSize = 5;
     private int messageId = 0;
 
-    /**
-     * Open Queue for sending messages
-     * @param purge existing messages in queue
-     */
-    public void openSendQueue(boolean purge){
-
-        try {
-            sendChannel = setUpQueueOpening(sendQueueName, purge);
-            sendChannel.queueDeclare(sosQueueName, false, false, false, null);
-            sendChannel.queuePurge(sosQueueName);
-        } catch (IOException | TimeoutException e) {
-            e.printStackTrace();
-        }
-
+    public CommunicationHandler(String sendQueueName, String receiveQueueName) {
+        this.sendQueueName = sendQueueName;
+        this.receiveQueueName = receiveQueueName;
     }
-    /**
-     * Open Queue for receiving messages
-     * @param purge existing messages in queue
-     * @param sos open an additional sos queue to receive messages from
-     */
-    public void openReceiveQueue(boolean purge, boolean sos){
-        try {
-            receiveChannel = setUpQueueOpening(receiveQueueName, purge);
-            receiveChannel.basicConsume(receiveQueueName, false, this::onReceiveCallback, consumerTag -> { });
-            if (sos){
-                receiveChannel.basicConsume(sosQueueName, false, this::onReceiveCallback, consumerTag -> { });
-            }
-        } catch (IOException | TimeoutException e) {
-            e.printStackTrace();
-        }
 
+    public CommunicationHandler(String sendQueueName, String receiveQueueName, DeliverCallback myCallback) {
+        this.sendQueueName = sendQueueName;
+        this.receiveQueueName = receiveQueueName;
+        this.myCallback = myCallback;
     }
 
 //    /**
@@ -66,33 +43,11 @@ public class CommunicationHandler implements ICommunication{
 //            sosChannel.basicConsume("sos", true, this::onReceiveCallback, consumerTag -> {});
 //        }
 //    }
-    /**
-     * Initiate new connection if necessary.
-     * Close channel if it was already open, and create new one.
-     * Purge queue if requested
-     * @param queueName name of new queue
-     * @param purge existing messages on queue
-     * @return the channel with its opened queue
-     * @throws IOException on connection error
-     * @throws TimeoutException on no response from RabbitMQ server
-     */
-    private Channel setUpQueueOpening(String queueName, boolean purge) throws IOException, TimeoutException {
-        if (connection == null){
-            connection = factory.newConnection();
-        }
 
-        Channel channel = connection.createChannel();
-        channel.basicQos(1);
-        Map<String, Object> args = Map.of("x-max-length", queueSize);
-        channel.queueDeclare(queueName, false, false, false, args);
-
-        if (purge){
-            channel.queuePurge(queueName);
-        }
-
-        return channel;
+    public CommunicationHandler(DeliverCallback myCallback) {
+        this.myCallback = myCallback;
     }
-//
+
 //    /**
 //     * Close Send queue
 //     * @throws IOException on connection error
@@ -111,49 +66,103 @@ public class CommunicationHandler implements ICommunication{
 //        receiveChannel.close();
 //    }
 
+    public CommunicationHandler() {
+    }
+
+    /**
+     * Open Queue for sending messages
+     *
+     * @param purge existing messages in queue
+     * @throws IOException      on connection error
+     * @throws TimeoutException on no response from RabbitMQ server
+     */
+    public void openSendQueue(boolean purge) throws IOException, TimeoutException {
+        sendChannel = setUpQueueOpening(sendQueueName, purge);
+        sendChannel.queueDeclare(sosQueueName, false, false, false, null);
+        sendChannel.queuePurge(sosQueueName);
+    }
+
+    /**
+     * Open Queue for receiving messages
+     *
+     * @param purge existing messages in queue
+     * @param sos   open an additional sos queue to receive messages from
+     * @throws IOException      on connection error
+     * @throws TimeoutException on no response from RabbitMQ server
+     */
+    public void openReceiveQueue(boolean purge, boolean sos) throws IOException, TimeoutException {
+        receiveChannel = setUpQueueOpening(receiveQueueName, purge);
+        receiveChannel.basicConsume(receiveQueueName, false, this::onReceiveCallback, consumerTag -> {
+        });
+        if (sos) {
+            receiveChannel.basicConsume(sosQueueName, false, this::onReceiveCallback, consumerTag -> {
+            });
+        }
+    }
+
+    /**
+     * Initiate new connection if necessary.
+     * Close channel if it was already open, and create new one.
+     * Purge queue if requested
+     *
+     * @param queueName name of new queue
+     * @param purge     existing messages on queue
+     * @return the channel with its opened queue
+     * @throws IOException      on connection error
+     * @throws TimeoutException on no response from RabbitMQ server
+     */
+    private Channel setUpQueueOpening(String queueName, boolean purge) throws IOException, TimeoutException {
+        if (connection == null) {
+            connection = factory.newConnection();
+        }
+
+        Channel channel = connection.createChannel();
+        channel.basicQos(1);
+        Map<String, Object> args = Map.of("x-max-length", queueSize);
+        channel.queueDeclare(queueName, false, false, false, args);
+
+        if (purge) {
+            channel.queuePurge(queueName);
+        }
+
+        return channel;
+    }
+
     /**
      * Close Send and Receive connection
+     *
+     * @throws IOException      on connection error
+     * @throws TimeoutException on no response from RabbitMQ server
      */
-    public void closeConnection() {
-        try {
-            if (sendChannel != null){
-                sendChannel.close();
-            }
-            if (receiveChannel != null){
-                receiveChannel.close();
-            }
-            if (connection != null){
-                connection.close();
-            }
-        } catch (IOException | TimeoutException e) {
-            e.printStackTrace();
-        }
+    public void closeConnection() throws IOException, TimeoutException {
+        sendChannel.close();
+        receiveChannel.close();
+        connection.close();
     }
 
     /**
      * Put message in Send queue
+     *
      * @param message to send
-     * @param sos send this message in the sos queue
+     * @param sos     send this message in the sos queue
+     * @throws IOException on connection error
      */
-    public void send(String message, boolean sos) {
+    public void send(String message, boolean sos) throws IOException {
         String queueName = sos ? sosQueueName : sendQueueName;
-        try {
-            sendChannel.basicPublish("", queueName, new AMQP.BasicProperties.Builder()
-                    .messageId(String.valueOf(messageId))
-                    .build(),
-                    message.getBytes());
-            messageId++;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        sendChannel.basicPublish("", queueName, new AMQP.BasicProperties.Builder()
+                        .messageId(String.valueOf(messageId))
+                        .build(),
+                message.getBytes());
+        messageId++;
     }
 
     /**
      * Default callback for receiving messages
+     *
      * @param consumerTag Rabbimq consumer tag
-     * @param delivery object containing message and data
+     * @param delivery    object containing message and data
      */
-    private void onReceiveCallback(String consumerTag, Delivery delivery){
+    private void onReceiveCallback(String consumerTag, Delivery delivery) {
         try {
             myCallback.handle(consumerTag, delivery);
         } catch (IOException e) {
@@ -166,54 +175,36 @@ public class CommunicationHandler implements ICommunication{
         }
     }
 
-    private void defaultCallback(String consumerTag, Delivery delivery){
+    private void defaultCallback(String consumerTag, Delivery delivery) {
         String json = new String(delivery.getBody(), StandardCharsets.UTF_8);
         System.out.println(json);
-    }
-
-    public CommunicationHandler(String sendQueueName, String receiveQueueName){
-        this.sendQueueName = sendQueueName;
-        this.receiveQueueName = receiveQueueName;
-    }
-
-    public CommunicationHandler(String sendQueueName, String receiveQueueName, DeliverCallback myCallback){
-        this.sendQueueName = sendQueueName;
-        this.receiveQueueName = receiveQueueName;
-        this.myCallback = myCallback;
-    }
-
-    public CommunicationHandler(DeliverCallback myCallback){
-        this.myCallback = myCallback;
-    }
-
-    public CommunicationHandler(){
     }
 
     public String getSendQueueName() {
         return sendQueueName;
     }
 
-    public String getReceiveQueueName() {
-        return receiveQueueName;
-    }
-
-    public DeliverCallback getMyCallback() {
-        return myCallback;
-    }
-
     public void setSendQueueName(String sendQueueName) {
         this.sendQueueName = sendQueueName;
+    }
+
+    public String getReceiveQueueName() {
+        return receiveQueueName;
     }
 
     public void setReceiveQueueName(String receiveQueueName) {
         this.receiveQueueName = receiveQueueName;
     }
 
+    public DeliverCallback getMyCallback() {
+        return myCallback;
+    }
+
     public void setCallback(DeliverCallback myCallback) {
         this.myCallback = myCallback;
     }
 
-    public void setCredentials(String host, String username, String password){
+    public void setCredentials(String host, String username, String password) {
         factory.setHost(host);
         factory.setUsername(username);
         factory.setPassword(password);
---
+++ src/main/java/Communication/ICommunication.java
@@ -0,0 +1,20 @@
+package Communication;
+
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public interface ICommunication {
+    void openSendQueue(boolean purge) throws IOException, TimeoutException;
+
+    void openReceiveQueue(boolean purge, boolean sos) throws IOException, TimeoutException;
+
+    void closeConnection() throws IOException, TimeoutException;
+
+    void send(String message, boolean sos) throws IOException;
+
+    void setCredentials(String host, String username, String password) throws IOException, TimeoutException;
+
+    void setCallback(DeliverCallback myCallback);
+}
